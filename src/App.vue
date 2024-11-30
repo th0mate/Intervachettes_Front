6dcf64930e249942a0322511c67299d6ee4ecd6a
th0mate <thomasloye1@gmail.com>
2024-12-01 00:13:23 +0100

    feat : init vueAccueil

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,20 @@ import {RouterLink, RouterView} from 'vue-router'
   <nav>
     <img @click="$router.push({name: 'accueil'})" src="@/assets/img/intervachettes_logo.png" alt="logo" class="logo"/>
     <div class="wrap-redirection">
-      <span @click="$router.push({name: 'accueil'})">ACCUEIL</span>
-      <span @click="$router.push({name: 'accueil'})">ÉVÉNEMENTS</span>
+      <RouterLink to="/accueil" class="nav-link" active-class="active-link">ACCUEIL</RouterLink>
+      <RouterLink to="/evenements" class="nav-link" active-class="active-link">ÉVÉNEMENTS</RouterLink>
       <div class="bouton border-bleu"><i class="fi fi-tr-circle-user"></i>Connexion</div>
       <div class="bouton fond-bleu"><i class="fi fi-tr-add"></i>Inscription</div>
     </div>
   </nav>
 
-  <RouterView class="contenu"/>
+  <div class="contenu-intervachettes">
+    <RouterView class="contenu"/>
+  </div>
 
   <footer>
     <span>©2024 Levy Vergnes Audouy Loye</span>
-    <img src="@/assets/img/intervachettes_logo.png" alt="logo" class="logo"/>
+    <img @click="$router.push({name: 'accueil'})" src="@/assets/img/intervachettes_logo.png" alt="logo" class="logo"/>
   </footer>
 </template>
 
