b96227f20ba33abfef34559fb414210a9c8412b3
loyet <thomas.loye@etu.umontpellier.fr>
2024-12-02 23:38:21 +0100

    feat : avancées sur les formulaires

--- src/views/inscription.vue
+++ src/views/inscription.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-  <h1>Inscription</h1>
+  <section class="connexion-inscription">
+
+    <div class="wrap-connexion-inscription">
+      <div class="illustation-login">
+        <img src="@/assets/img/intervachettes_logo.png" alt="logo" class="logo"/>
+        <img src="@/assets/img/illuConnexion.png" alt="illustration"/>
+      </div>
+
+      <div class="form-login">
+        <h1>Inscription</h1>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <style scoped>
-
+@import "@/assets/styles/styleConnexionInscription.css";
 </style>
