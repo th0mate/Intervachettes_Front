c3de2e6d0e8ed7e7f541af55d9cc1f33af1e9284
loyet <thomas.loye@etu.umontpellier.fr>
2024-12-11 13:55:55 +0100

    feat : init vueSingleEvenement

--- src/views/singleEvenement.vue
+++ src/views/singleEvenement.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import {useRoute} from 'vue-router'
 import type {Evenement} from "@/types";
-import {ref, type Ref} from "vue";
+import {onMounted, ref, type Ref} from "vue";
+import {afficherCarteGoogleMaps} from "@/assets/js/gestionGoogleMapsAPI.js";
+
 
 const route = useRoute()
 const id = route.params.id
 
-const evenement: Ref<Evenement[]> = ref(
+const evenement: Ref<Evenement> = ref(
   {
     id: 1,
     nom: "Lyon",
@@ -16,6 +18,11 @@ const evenement: Ref<Evenement[]> = ref(
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus et lectus vitae facilisis. Pellentesque a nulla hendrerit, maximus nisi eu, vehicula tellus. Duis interdum purus elementum tincidunt aliquam. Donec ipsum odio, tincidunt eget ipsum sit amet, pharetra tristique tortor. Suspendisse vestibulum ullamcorper nunc, sed sodales arcu feugiat non. Maecenas ut nunc in felis euismod tincidunt et in tellus. Sed magna augue, viverra vitae neque ultricies, fermentum ullamcorper odio. Quisque elementum lacus eu urna convallis dictum. Fusce eu mattis sapien. Cras semper accumsan sapien in vestibulum. Nam congue odio quis nibh tincidunt, ac facilisis lectus cursus. Proin pellentesque neque vel malesuada lacinia.",
     prix: 0,
   });
+
+onMounted(async () => {
+  await afficherCarteGoogleMaps(evenement.value.lieu, `map`);
+});
+
 </script>
 
 <template>
