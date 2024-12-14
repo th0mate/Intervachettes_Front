14f060278cc5fe1091be804a8960824d8d1bdea5
loyet <thomas.loye@etu.umontpellier.fr>
2024-12-14 21:29:27 +0100

    feat : front vue création event (sans responsive)

--- src/views/creationEvenement.vue
+++ src/views/creationEvenement.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts">
 
+const nom = ref("");
+const description = ref("");
+const adresse = ref("");
+const dateDebut = ref("");
+const dateFin = ref("");
+
+const envoyer = () => {
+  /* TODO
+  apiStore.createRessource('evenements', {message: message.value})
+    .then(() => {
+      notify({
+        type: 'success',
+        title: 'Événement créé avec succès',
+      });
+      message.value = '';
+      emit('updated');
+    });
+   */
+}
+
+
 </script>
 
 <template>
@@ -27,8 +48,31 @@
 
         <label>
           <span class="texte-gris-simple">Nom de l'événement</span>
-          <span class="texte-description color-blue">Le nom de la ville dans laquelle se déroulera l’événement</span>
-          <input type="text" v-model="nom" required>
+          <span class="texte-description color-blue"><i class="fi fi-rr-info"></i> Le nom de la ville dans laquelle se déroulera l’événement</span>
+          <input type="text" v-model="nom" placeholder="Montpellier" required>
+        </label>
+
+        <label>
+          <span class="texte-gris-simple">Description de l'événement</span>
+          <span class="texte-description color-blue"><i class="fi fi-rr-info"></i> Doit contenir le nombre d’équipes qui vont participer, et le
+            nombre de spectateurs maximal, et d'autres informations sur l'événement.</span>
+          <textarea v-model="description" placeholder="“Venez à Montpellier pour voir les équipes de Béziers, Montpellier, Nîmes et Narbonne s’affronter ! 2000 spectateurs maximum, 15 participants par ville maximum”." required></textarea>
+        </label>
+
+        <label>
+          <span class="texte-gris-simple">Adresse de l'événement</span>
+          <span class="texte-description color-blue"><i class="fi fi-rr-info"></i> L'adresse complète de l'emplacement dans lequel se déroulera l'événement</span>
+          <input type="text" v-model="nom" placeholder="99 Avenue d'Occitanie, 34000 Montpellier" required>
+        </label>
+
+        <label>
+          <span class="texte-gris-simple">Date et heure de début de l'événement</span>
+          <input type="datetime-local" v-model="dateDebut" required>
+        </label>
+
+        <label>
+          <span class="texte-gris-simple">Date et heure de fin de l'événement</span>
+          <input type="datetime-local" v-model="dateFin" required>
         </label>
 
         <div class="wrap-bouton">
