3757d175fe9f4ec3dee61d702acb9f6d370109b3
vergn <vergnes.lorick@gmail.com>
2024-12-21 16:42:32 +0100

    feat : rajoute de vérification pour s'inscrire

--- src/views/singleEvenement.vue
+++ src/views/singleEvenement.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {useRoute} from 'vue-router'
-import type {Evenement} from "@/types";
+import type {Evenement, Inscription} from "@/types";
 import {ref, type Ref} from "vue";
 import {apiStore} from "@/util/apiStore";
 import GoogleMaps from "@/components/GoogleMaps.vue";
@@ -10,23 +10,72 @@ import {notify} from "@kyvg/vue3-notification";
 const route = useRoute()
 const id = route.params.id
 
-const nbParticipants:Ref<Inscription[]> = ref([]);
+const inscriptions: Ref<Inscription[]> = ref([]);
+const inscriptionsUserConnect: Ref<Inscription[]> = ref([]);
 
 apiStore.getAll(`inter_vachettes/${id}/inscriptions`)
   .then(reponseJSON => {
-    nbParticipants.value = reponseJSON.member;
+    inscriptions.value = reponseJSON.member;
   });
 
-const evenement:Ref<Evenement[]> = ref('Chargement');
+function chargerEvenements() {
+  if (apiStore.estConnecte) {
+    apiStore.getRessourceConnected('utilisateurs/' + apiStore.utilisateurConnecte.id + '/inscriptions')
+      .then(reponseJSON => {
+        inscriptionsUserConnect.value = reponseJSON['member'];
+      });
+  }
+}
+
+chargerEvenements();
+
+const evenement: Ref<Evenement[]> = ref('Chargement');
 apiStore.getById('inter_vachettes', id)
   .then(reponseJSON => {
     evenement.value = reponseJSON;
   });
 
+function hasDateConflict(newEvent) {
+  return inscriptionsUserConnect.value.some(inscription => {
+    const event = inscription.evenements;
+    const newEventDebut = new Date(newEvent.dateDebutEvenement);
+    const newEventFin = new Date(newEvent.dateFinEvenement);
+    const existingEventDebut = new Date(event.dateDebutEvenement);
+    const existingEventFin = new Date(event.dateFinEvenement);
+
+    return (newEventDebut < existingEventFin && newEventFin > existingEventDebut);
+  });
+}
+
 function inscrireUtilisateur() {
-  if (apiStore.estConnecte) {
+  if (!apiStore.estConnecte) {
+    notify({
+      type: 'warning',
+      title: 'Utilisateur non connecté',
+      text: 'Veuillez vous connecter pour vous inscrire à un événement.',
+      group: 'custom-template'
+    });
+  } else {
+    if (inscriptions.length >= evenement.value.nbParticipantsMax) {
+      notify({
+        type: 'warning',
+        title: 'Inscription échouée',
+        text: 'Le nombre maximum de participants est atteint.',
+        group: 'custom-template'
+      });
+      return;
+    }
+    if (hasDateConflict(evenement.value)) {
+      notify({
+        type: 'warning',
+        title: 'Inscription échouée',
+        text: 'Vous êtes déjà inscrit à un événement qui se déroule à la même date.',
+        group: 'custom-template'
+      });
+      return;
+    }
     const data = {
-      participant: `/InterVachettesAPI/public/api/utilisateurs/${apiStore.utilisateurConnecte}`,
+      participant: `/InterVachettesAPI/public/api/utilisateurs/${apiStore.utilisateurConnecte.id}`,
       evenements: `/InterVachettesAPI/public/api/inter_vachettes/${id}`
     };
     apiStore.createRessource("inscriptions", data)
@@ -40,28 +89,13 @@ function inscrireUtilisateur() {
           });
         } else {
           notify({
-            type: 'warn',
+            type: 'warning',
             title: 'Inscription échouée',
             text: response.error || 'Une erreur est survenue.',
             group: 'custom-template'
           });
         }
-      })
-      .catch(error => {
-        notify({
-          type: 'error',
-          title: 'Erreur',
-          text: `Une erreur est survenue : ${error.message || 'Action impossible'}`,
-          group: 'custom-template'
-        });
       });
-  } else {
-    notify({
-      type: 'warn',
-      title: 'Utilisateur non connecté',
-      text: 'Veuillez vous connecter pour vous inscrire à un événement.',
-      group: 'custom-template'
-    });
   }
 }
 
@@ -105,19 +139,24 @@ function inscrireUtilisateur() {
 
         <div class="chiffres-cles">
           <span class="chiffre-cle">
-            <span class="chiffre">{{ evenement.nbParticipantsMax}}</span>
+            <span class="chiffre">{{ evenement.nbParticipantsMax }}</span>
             <span>Places restantes</span>
           </span>
           <span class="chiffre-cle">
-            <span class="chiffre">{{ evenement.nbParticipantsMax - nbParticipants.length}}</span>
+            <span class="chiffre">{{ evenement.nbParticipantsMax - inscriptions.length }}</span>
             <span>Candidats inscrits</span>
           </span>
         </div>
 
         <span class="texte-gris-simple"><i
-          class="fi fi-rr-marker color-blue"></i> Du {{ new Date(evenement.dateDebutEvenement).toLocaleString("fr") }} au {{ new Date(evenement.dateFinEvenement).toLocaleString("fr") }}</span>
-        <span class="texte-gris-simple"><i class="fi fi-rr-calendar-clock color-blue"></i> {{ evenement.adresse }}</span>
-        <div @click="inscrireUtilisateur" class="bouton icon-animation">Inscription à l'événement<i class="fi fi-rr-arrow-right"></i>
+          class="fi fi-rr-marker color-blue"></i> Du {{ new Date(evenement.dateDebutEvenement).toLocaleString("fr") }} au {{
+            new Date(evenement.dateFinEvenement).toLocaleString("fr")
+          }}</span>
+        <span class="texte-gris-simple"><i class="fi fi-rr-calendar-clock color-blue"></i> {{
+            evenement.adresse
+          }}</span>
+        <div @click="inscrireUtilisateur" class="bouton icon-animation">Inscription à l'événement<i
+          class="fi fi-rr-arrow-right"></i>
         </div>
       </div>
 
@@ -128,7 +167,9 @@ function inscrireUtilisateur() {
           <div class="infos-organisateur">
             <div v-if="evenement.organisateur">
               <i class="fi fi-rr-user"></i>
-              <span class="texte-gris-simple">{{ evenement.organisateur.prenom }} {{evenement.organisateur.nom}}</span>
+              <span class="texte-gris-simple">{{
+                  evenement.organisateur.prenom
+                }} {{ evenement.organisateur.nom }}</span>
             </div>
             <div @click="TODO" class="bouton icon-animation">Voir le compte<i class="fi fi-rr-arrow-right"></i></div>
           </div>
