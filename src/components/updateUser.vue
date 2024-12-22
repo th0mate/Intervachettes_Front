be9c3a76f937adc8e496702fb566964c79c8f448
loyet <thomas.loye@etu.umontpellier.fr>
2024-12-22 17:10:05 +0100

    fix : lint updateUser

--- src/components/updateUser.vue
+++ src/components/updateUser.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import {apiStore} from "@/util/apiStore";
-import type {Utilisateur} from '@/types';
+import { ref } from 'vue';
+import { apiStore } from "@/util/apiStore";
+import type { Utilisateur } from '@/types';
 import { notify } from "@kyvg/vue3-notification";
 
 const props = defineProps<{ utilisateur: Utilisateur }>();
 const emit = defineEmits<{ updated: () => void }>();
 
+const localUtilisateur = ref({ ...props.utilisateur });
+
 const updateUser = () => {
-  apiStore.update('utilisateurs', props.utilisateur, apiStore.utilisateurConnecte, true)
+  apiStore.update('utilisateurs', localUtilisateur.value, apiStore.utilisateurConnecte, true)
     .then(() => {
       notify({
         type: 'success',
@@ -19,33 +22,33 @@ const updateUser = () => {
 </script>
 
 <template>
-  <div class="actionCompte" v-if="props.utilisateur">
+  <div class="actionCompte" v-if="localUtilisateur">
     <h1 class="grand-titre">Modifier le <span class="color-blue">compte</span></h1>
 
     <form @submit.prevent="updateUser" class="modifier-compte">
       <div>
         <label class="texte-gris-simple" for="adresseMail">Adresse mail :</label>
-        <input id="adresseMail" v-model="props.utilisateur.adresseMail" type="email"/>
+        <input id="adresseMail" v-model="localUtilisateur.adresseMail" type="email"/>
       </div>
       <div>
         <label class="texte-gris-simple" for="code_pages_vertes">Code PagesVertes :</label>
-        <input id="code_pages_vertes" v-model="utilisateur.code_pages_vertes" type="text"/>
+        <input id="code_pages_vertes" v-model="localUtilisateur.code_pages_vertes" type="text"/>
       </div>
       <div>
         <label class="texte-gris-simple" for="description">Description :</label>
-        <textarea id="description" v-model="props.utilisateur.biographie" type="text"/>
+        <textarea id="description" v-model="localUtilisateur.biographie" type="text"/>
       </div>
       <div>
         <label class="texte-gris-simple" for="nom">Nom :</label>
-        <input id="nom" v-model="props.utilisateur.nom" type="text"/>
+        <input id="nom" v-model="localUtilisateur.nom" type="text"/>
       </div>
       <div>
         <label class="texte-gris-simple" for="prenom">Prénom :</label>
-        <input id="prenom" v-model="props.utilisateur.prenom" type="text"/>
+        <input id="prenom" v-model="localUtilisateur.prenom" type="text"/>
       </div>
       <div>
         <label class="texte-gris-simple" for="plainPassword">Mot de Passe :</label>
-        <input class="texte-gris-simple" id="plainPassword" v-model="props.utilisateur.plainPassword" type="password"/>
+        <input class="texte-gris-simple" id="plainPassword" v-model="localUtilisateur.plainPassword" type="password"/>
       </div>
       <button type="submit" class="bouton icon-animation">Enregistrer <i
         class="fi fi-rr-arrow-right"></i></button>
